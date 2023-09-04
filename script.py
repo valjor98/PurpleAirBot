6415c56dbb94a0284c45cc058b175e5067a151a3
valjor98 <valjor98@gmail.com>
2023-09-04 08:50:45 -0600

    Added initial execution loop

--- script.py
+++ script.py
@@ -4,3 +4,23 @@ import pandas as pd
 import time
 
 
+# Pull data every 30 minutes
+while True:
+    # Fetch data from PurpleAir API
+    sensor_data = getSensorsData(sensor_ids, fields)
+    # Filter out old data
+    df = pd.DataFrame(sensor_data)
+    filtered_data = AGEfilter(df, 20)
+    # Insert data into SQLite database
+    print("data:", conn)
+    filtered_data.to_sql("SensorData", conn, if_exists="append", index=False)
+    print("Data inserted into database.")
+
+    # Query to fetch the most recent 5 records
+    cursor.execute("SELECT * FROM SensorData ORDER BY id DESC LIMIT 5")
+    rows = cursor.fetchall()
+
+    # Print the fetched records
+    for row in rows:
+        print(row)
+    time.sleep(1800)  # Wait for 30 minutes
