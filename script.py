8c317e9dfaf7363842ef663e9bb01067950af34f
valjor98 <valjor98@gmail.com>
2023-09-09 13:07:40 -0600

    Added mapping to avoid error when pulling data with fields that have a period in their name

--- script.py
+++ script.py
@@ -1,12 +1,39 @@
+#      
+
+
+import requests
+import sqlite3
+import pandas as pd
+import time
 import requests
 import sqlite3
 import pandas as pd
 import time
 
+# Mapping of API field names to SQLite column names
+field_mapping = {
+    "last_seen": "last_seen",
+    "pm2.5_alt": "pm25_alt",
+    "pm2.5_alt_a": "pm25_alt_a",
+    "pm2.5_alt_b": "pm25_alt_b",
+    "pm2.5": "pm25",
+    "pm2.5_a": "pm25_a",
+    "pm2.5_b": "pm25_b",
+    "pm2.5_atm": "pm25_atm",
+    "pm2.5_atm_a": "pm25_atm_a",
+    "pm2.5_atm_b": "pm25_atm_b",
+    "pm2.5_cf_1": "pm25_cf_1",
+    "pm2.5_cf_1_a": "pm25_cf_1_a",
+    "pm2.5_cf_1_b": "pm25_cf_1_b",
+    "temperature_a": "temperature_a",
+    "humidity_a": "humidity_a",
+    "pressure_a": "pressure_a"
+}
+
 # Function to get sensor data from PurpleAir API
 def getSensorsData(sensor_ids, fields):
     base_url = "https://api.purpleair.com/v1/sensors/"
-    api_key = "" # TODO change to be en env variable  
+    api_key = "" # TODO change to be en env variable 
     data_frames = []
 
     for sensor_id in sensor_ids:
@@ -17,6 +44,8 @@ def getSensorsData(sensor_ids, fields):
             sensor_data = response.json()["sensor"]
             selected_data = {field: sensor_data.get(field) for field in fields}
             df = pd.DataFrame([selected_data])
+            # Renaming columns
+            df.rename(columns=field_mapping, inplace=True)
             data_frames.append(df)
         else:
             print(f"Bad response for sensor {sensor_id}: {response.status_code}")
@@ -25,24 +54,31 @@ def getSensorsData(sensor_ids, fields):
 
 # Function to filter out sensor data that's older than a certain time
 def AGEfilter(df, max_minutes):
-    # Convert Unix timestamps to timezone-aware datetime objects
     df['last_seen'] = pd.to_datetime(df['last_seen'], unit='s').dt.tz_localize('UTC')
-
-    # Compute the age in minutes
     df['age'] = (pd.Timestamp.now(tz='UTC') - df['last_seen']).dt.total_seconds() / 60
-
-    # Filter based on age
     return df[df['age'] <= max_minutes]
 
 # Initialize SQLite database
 conn = sqlite3.connect("purpleair_data.db")
 cursor = conn.cursor()
+
+# Note: The column names here should match the SQLite-friendly names in the mapping
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS SensorData (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     last_seen DATETIME,
-    pm2_5_60minute_a REAL,
-    pm2_5_60minute_b REAL,
+    pm25_alt REAL,
+    pm25_alt_a REAL,
+    pm25_alt_b REAL,
+    pm25 REAL,
+    pm25_a REAL,
+    pm25_b REAL,
+    pm25_atm REAL,
+    pm25_atm_a REAL,
+    pm25_atm_b REAL,
+    pm25_cf_1 REAL,
+    pm25_cf_1_a REAL,
+    pm25_cf_1_b REAL,
     temperature_a REAL,
     humidity_a REAL,
     pressure_a REAL,
@@ -52,25 +88,21 @@ CREATE TABLE IF NOT EXISTS SensorData (
 conn.commit()
 
 # List of sensor IDs and fields
-sensor_ids = ["36721"]  # TODO change to sensors we want
-fields = ["last_seen", "pm2_5_60minute_a", "pm2_5_60minute_b", "temperature_a", "humidity_a", "pressure_a"]
+sensor_ids = ["37747"]  # TODO: change to sensors you want
+fields = list(field_mapping.keys())  # This will take the keys from the field_mapping dictionary
 
 # Pull data every 30 minutes
 while True:
-    # Fetch data from PurpleAir API
     sensor_data = getSensorsData(sensor_ids, fields)
-    # Filter out old data
     df = pd.DataFrame(sensor_data)
     filtered_data = AGEfilter(df, 20)
-    # Insert data into SQLite database
     filtered_data.to_sql("SensorData", conn, if_exists="append", index=False)
     print("Data inserted into database.")
 
-    # Query to fetch the most recent 5 records
     cursor.execute("SELECT * FROM SensorData ORDER BY id DESC LIMIT 5")
     rows = cursor.fetchall()
 
-    # print the fetched data every time there's an insert
     for row in rows:
         print(row)
+
     time.sleep(1800)  # Wait for 30 minutes
